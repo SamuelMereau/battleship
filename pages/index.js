2af45727ebf866195a868e41d00b06a62caaac9d
Samuel Mereau <samuelmereau@gmail.com>
2021-12-27 19:18:50 +1100

    Host Game menu

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,21 @@ import styles from '../styles/Home.module.css'
 import { useState } from 'react';
 
 export default function Home() {
+  const [hostMenuActive, setHostMenuActive] = useState(false);
   const [joinMenuActive, setJoinMenuActive] = useState(false);
+
+  const hostMenu = hostMenuActive ? `${styles.slideIn} ${styles.hostMenu}` : styles.hostMenu;
   const joinMenu = joinMenuActive ? `${styles.slideIn} ${styles.joinMenu}` : styles.joinMenu;
-  const mainMenu = joinMenuActive ? `${styles.slideOut} ${styles.mainMenu}` : styles.mainMenu;
+  const mainMenu = () => {
+    switch (true) {
+      case joinMenuActive:
+        return `${styles.slideOutLeft} ${styles.mainMenu}`;
+      case hostMenuActive:
+        return `${styles.slideOutRight} ${styles.mainMenu}`;
+      default:
+        return styles.mainMenu;
+    }
+  }
 
   return (
     <>
@@ -16,16 +28,84 @@ export default function Home() {
       </Head>
       <Header/>
       <main id={styles.mainContent}>
-        <div className={mainMenu}>
+        {/* Host Menu */}
+        <div id='hostMenu' className={hostMenu}>
+        <div className={styles.menuHeader}>
+            <h1 className={styles.pageHeader}>Host a Game</h1>
+            <svg onClick={() => setHostMenuActive(false)} xmlns="http://www.w3.org/2000/svg" 
+                 className={`${styles.isPointingRight} ${styles.backArrow}`} 
+                 viewBox="0 0 24 24" 
+                 strokeWidth="1.5" 
+                 stroke="#000000" 
+                 fill="none" 
+                 strokeLinecap="round" 
+                 strokeLinejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+              <line x1="5" y1="12" x2="19" y2="12" />
+              <line x1="5" y1="12" x2="11" y2="18" />
+              <line x1="5" y1="12" x2="11" y2="6" />
+            </svg>
+          </div>
+            <form id='hostGameOptionsMenu' className={styles.hostGameForm}>
+              <div id={styles.team}>
+                <label htmlFor='colourOptions'>Your Team</label>
+                <select name='colourOptions' id='colourOptions'>
+                  <option value='blue'>Blue Team</option>
+                  <option value='red'>Red Team</option>
+                </select>
+              </div>
+              <div id={styles.commands}>
+                <label>Commands Per Row</label>
+                <div className={styles.commandsOptions}>
+                  <label htmlFor='startRowCommands'>Start Row Commands</label>
+                  <input type='text' name='startRowCommands' id='startRowCommands' className='smallTextField' defaultValue='2'></input>
+                  <label htmlFor='middleRowCommands'>Middle Row Commands</label>
+                  <input type='text' name='middleRowCommands' id='middleRowCommands' className='smallTextField' defaultValue='3'></input>
+                  <label htmlFor='mineRowCommands'>Mine Row Commands</label>
+                  <input type='text' name='mineRowCommands' id='mineRowCommands' className='smallTextField' defaultValue='4'></input>
+                </div>
+              </div>
+              <div id={styles.options}>
+                <div className={styles.option}>
+                  <label htmlFor='allowSpectators'>Allow Spectators</label>
+                  <label className='switch'>
+                    <input type='checkbox' defaultChecked={true} name='allowSpectators' id='allowSpectators'></input>
+                    <span className='switch-slider'></span>
+                  </label>
+                </div>
+                <div className={styles.option}>
+                  <label htmlFor='enableTutorial'>Enable Tutorial</label>
+                  <label className='switch'>
+                    <input type='checkbox' name='enableTutorial' id='enableTutorial'></input>
+                    <span className='switch-slider'></span>
+                  </label>
+                </div>
+              </div>
+              <input type='button' id='resetToDefault' value='Reset to Default' className='smallButton'></input>
+            </form>
+            <button className='isMain'>Start Game</button>
+        </div>
+
+        {/* Main Menu */}
+        <div className={mainMenu()}>
           <h1 className={styles.pageHeader}>Join or Host a game</h1>
-          <div className={styles.buttonList}>
-            <button className='isMain'>Host a Game</button>
+          <div className={styles.optionsMenu}>
+            <button onClick={() => setHostMenuActive(!hostMenuActive)} className='isMain'>Host a Game</button>
             <button onClick={() => setJoinMenuActive(!joinMenuActive)}>Join a Game</button>
           </div>
         </div>
+        
+        {/* Join Menu */}
         <div id='joinMenu' className={joinMenu}>
-          <div id={styles.joinMenuHeader}>
-            <svg onClick={() => setJoinMenuActive(false)} xmlns="http://www.w3.org/2000/svg" className={styles.backArrow} viewBox="0 0 24 24" strokeWidth="1.5" stroke="#000000" fill="none" strokeLinecap="round" strokeLinejoin="round">
+          <div className={styles.menuHeader}>
+            <svg onClick={() => setJoinMenuActive(false)} xmlns="http://www.w3.org/2000/svg" 
+                 className={styles.backArrow} 
+                 viewBox="0 0 24 24" 
+                 strokeWidth="1.5" 
+                 stroke="#000000" 
+                 fill="none" 
+                 strokeLinecap="round" 
+                 strokeLinejoin="round">
               <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
               <line x1="5" y1="12" x2="19" y2="12" />
               <line x1="5" y1="12" x2="11" y2="18" />
@@ -33,7 +113,7 @@ export default function Home() {
             </svg>
             <h1 className={styles.pageHeader}>Join a Game</h1>
           </div>
-          <div className={styles.buttonList}>
+          <div id='joinGameOptionsMenu' className={styles.optionsMenu}>
             <input type='text' id='joincode' placeholder='Join Code'></input>
             <input type='text' id='username' placeholder='Username'></input>
             <button className='isMain'>Join</button>
